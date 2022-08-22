be2960aba29717182e7b959df855f6473e35cef2
Fabian Portilla <fas_456@hotmail.com>
2022-08-22 12:37:06 -0500

    Add button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,35 @@
 
 import React from 'react'
+
+import { IconButton } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material'
+
 import { JournalLayout } from '../layout/JournalLayout'
-import { NoteView } from '../views'
+import { NothingSelectedView, NoteView } from '../views'
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
 
-            {/* <NothingSelectedView /> */}
-            <NoteView />
+            <NothingSelectedView />
+            {/* <NoteView /> */}
+
+            <IconButton
+                size='large'
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': {
+                        backgroundColor: 'error.main', opacity: 0.9
+                    },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50,
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }} />
+
+            </IconButton>
         </JournalLayout>
     )
 }
