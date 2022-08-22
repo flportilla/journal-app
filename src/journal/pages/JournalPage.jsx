14f33e3c94bcaae286742b64098a5c5a354a0d3e
Fabian Portilla <fas_456@hotmail.com>
2022-08-22 10:31:45 -0500

    Add Journal page dummy

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,8 +1,14 @@
 import { Typography } from '@mui/material'
 import React from 'react'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
     return (
-        <Typography>JournalPage</Typography>
+        <JournalLayout>
+
+            <Typography>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quisquam? Ad excepturi laborum alias reprehenderit, aspernatur voluptatum esse! Reiciendis adipisci laborum voluptates quis id tempore in aliquam ea placeat minima quia, rem saepe repellendus ad ullam voluptatibus beatae, molestiae architecto dolore a qui, fugiat impedit commodi. Beatae odit inventore quia praesentium unde, natus quos. Ea, cupiditate vel consequuntur a velit hic eveniet sit illo repellat, odit modi suscipit aliquid! Exercitationem minus in qui a error? Facilis, ut amet voluptates fugiat cupiditate aliquid exercitationem hic molestias molestiae? Quibusdam ab, voluptate aut dolores numquam quia amet corporis et veniam magni? Deleniti, fuga?
+            </Typography>
+        </JournalLayout>
     )
 }
